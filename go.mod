35b3771d890439c618d7d09c9b0e16e784fed576
itallominatti <itallo.monteiro@propig.com.br>
2026-09-10 17:44:23 -0300

    feat: add Account struct and methods for account management

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module simple_bank
 
 go 1.27.1
+
+require github.com/google/uuid v1.6.0
---
+++ internal/domain/account.go
@@ -0,0 +1,54 @@
+package domain
+
+import (
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Account struct {
+	id        string
+	ownerName string
+	balance   Money
+	createdAt time.Time
+	updatedAt time.Time
+}
+
+func NewAccount(ownerName string) (*Account, error) {
+	ownerName = strings.TrimSpace(ownerName)
+	if ownerName == "" {
+		return nil, ErrInvalidOwnerName
+	}
+
+	now := time.Now().UTC()
+	return &Account{
+		id:        uuid.NewString(),
+		ownerName: ownerName,
+		balance:   0,
+		createdAt: now,
+		updatedAt: now,
+	}, nil
+}
+
+func RestoreAccount(id, ownerName string, balance Money, createdAt, updatedAt time.Time) *Account {
+	return &Account{
+		id:        id,
+		ownerName: ownerName,
+		balance:   balance,
+		createdAt: createdAt,
+		updatedAt: updatedAt,
+	}
+}
+
+func (a *Account) Deposit(amount Money) error {
+	if !amount.IsPositive() {
+		return ErrInvalidAmount
+	}
+	if a.balance < amount {
+		return ErrInsufficientFunds
+	}
+	a.balance -= amount
+	a.updatedAt = time.Now().UTC()
+	return nil
+}
