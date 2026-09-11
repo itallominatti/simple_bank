a879653ec18c6c3ae0ec77bdf55911d73acc2c4c
itallominatti <itallo.monteiro@propig.com.br>
2026-09-11 09:58:28 -0300

    feat: implement Transfer function for account transactions

--- internal/domain/transfer.go
+++ internal/domain/transfer.go
@@ -1,1 +1,13 @@
 package domain
+
+func Transfer(from, to *Account, amount Money) error {
+	if from.ID() == to.ID() {
+		return ErrSameAccountTransfer
+	}
+
+	if err := from.Withdraw(amount); err != nil {
+		return err
+	}
+
+	return to.Deposit(amount)
+}
