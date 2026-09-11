3b115bda689979cacba297ddb50211877000d1e3
itallominatti <itallo.monteiro@propig.com.br>
2026-09-11 10:38:26 -0300

    feat: implement UnitOfWork pattern with Repositories struct for account and transaction management

--- internal/application/unit_of_work.go
+++ internal/application/unit_of_work.go
@@ -1,1 +1,15 @@
 package application
+
+import (
+	"context"
+	"simple_bank/internal/domain"
+)
+
+type Repositories struct {
+	Accounts     domain.AccountRepository
+	Transactions domain.TransactionRepository
+}
+
+type UnitOfWork interface {
+	Do(ctx context.Context, fn func(repos Repositories) error) error
+}
