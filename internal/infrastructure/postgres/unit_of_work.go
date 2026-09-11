66112f6041920b9b960137d947a196905389e133
itallominatti <itallo.monteiro@propig.com.br>
2026-09-11 14:07:18 -0300

    feat: implement UnitOfWork for transaction management with context support

---
+++ internal/infrastructure/postgres/unit_of_work.go
@@ -0,0 +1,35 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"simple_bank/internal/application"
+)
+
+type UnitOfWork struct {
+	db *sql.DB
+}
+
+func (u *UnitOfWork) Do(ctx context.Context, fn func(repos application.Repositories) error) error {
+	tx, err := u.db.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("Iniciar transação: %w", err)
+	}
+
+	defer func() { _ = tx.Rollback() }()
+
+	repos := application.Repositories{
+		Accounts:     NewAccountRepository(tx),
+		Transactions: NewTransactionRepository(tx),
+	}
+
+	if err := fn(repos); err != nil {
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("Confirmar transação: %w", err)
+	}
+	return nil
+}
